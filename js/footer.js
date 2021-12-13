ebb5a5c05a77fda3cdfc45ba40a4762d67471c31
alvaro <alsato.650cc@gmail.com>
2021-12-13 17:38:22 -0500

    asdasd

---
+++ js/footer.js
@@ -0,0 +1,102 @@
+const footer = document.querySelector("#footer");
+footer.innerHTML = 
+`
+<!-- Footer -->
+<footer class="text-center text-lg-start text-muted footer-style">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+            
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="mb-extend">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-sm-12 col-md-6 p-4 col-xl-5">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <img src="./images/logos/red1a1-blanco.png" alt="...">
+                    </h6>
+                    <p class="footer-content">
+                        Red 1a1 es la solución para asesores independientes e inmobiliarias que permite la comunicación y 
+                        el intercambio de carteras de clientes y propiedades. 
+                        A través de esta herramienta versátil podrás agregar, buscar y compartir propiedades.
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-sm-12 col-md-6 p-4 col-xl-2">
+                    <!-- Links -->
+                    <h5 class="fw-bold text-white mb-4">
+                        Secciones
+                    </h5>
+                    <p class="">
+                        <a href="#!" class="footer-link">Nosotros</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="footer-link">Paquetes</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="footer-link">Contáctanos</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="footer-link">Registro</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-sm-12 col-md-6 p-4 col-xl-2">
+                    <!-- Links -->
+                    <h5 class="fw-bold text-white mb-4">
+                        Nuestras Oficinas
+                    </h5>
+                    <p class="footer-content">
+                        Querétaro, México. C.P. 76230
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-sm-12 col-md-6 p-4 col-xl-3">
+                    <!-- Links -->
+                    <h5 class="fw-bold text-white mb-4">
+                        Teléfono
+                    </h5>
+                    <p class="footer-content">
+                        <a class="footer-phone" href="tel:442 543 9190">442 543 9190</a>
+                    </p>
+                    <div class="icons">
+                        <a class="me-2" href="#"><img src="./images/icons/Facebook.png" alt=""></a>
+                        <a href="#"><img src="./images/icons/WhatsApp.png" alt=""></a>
+                    </div>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        Copyright © 2021:<a class="text-reset fw-bold" style="text-decoration: none;" href="#"> Red 1 a 1</a>. Todos los derechos reservados.
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
+`
---
+++ js/navbar.js
@@ -0,0 +1,25 @@
+const whitebar = document.querySelector("#whitebar");
+whitebar.innerHTML = 
+`
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="./images/logos/red1a1-color.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+            <div class="d-flex">
+                <a class="nav-enlace" data-bs-toggle="modal" href="#exampleModalToggle2"
+                    role="button">Regístrate
+                </a>
+                <a class="sesion-btn mb-2 mb-lg-0" data-bs-toggle="modal" href="#exampleModalToggle"
+                    role="button">Inicia Sesión
+                </a>
+                <a class="nav-perfil ms-3" data-bs-toggle="modal" href="#"
+                    role="button"><img src="./images/icons/perfil.png" alt=".." width="100%">
+                </a>
+            </div>
+        </div>
+    </div>
+`
