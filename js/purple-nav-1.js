9ed0514c787fb37c7b0bc4bddaa761750e407d8b
alvaro <alsato.650cc@gmail.com>
2021-12-15 14:48:09 -0500

    purple nav

---
+++ js/purple-nav-1.js
@@ -0,0 +1,23 @@
+const footer = document.querySelector("#purple-nav-1");
+footer.innerHTML = 
+`
+    <div class="purple-nav-2">
+        <ul class="nav d-flex justify-content-center">
+            <li class="nav-item">
+                <a class="purple-links me-5" href="#">Mis Propiedades</a>
+            </li>
+            <li class="nav-item">
+                <a class="purple-links me-5" href="#">Mis Favoritos</a>
+            </li>
+            <li class="nav-item">
+                <a class="purple-links me-5" href="#">Mi Cuenta</a>
+            </li>
+            <li class="nav-item">
+                <a class="purple-links me-5" href="#">Historial de Inmuebles</a>
+            </li>
+            <li class="nav-item">
+                <a class="purple-img-link"5href="#"><img src="./images/icons/notify.png" alt="..."></a>
+            </li>
+        </ul>
+    </div>
+`
